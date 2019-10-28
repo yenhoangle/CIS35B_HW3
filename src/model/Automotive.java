efb2f296f626c4f3105eebabfae0420116b28aed
yenhoangle <yhle@ucsd.edu>
2019-10-28 00:25:48 -0700

    added some getter methods for Automotive

--- src/model/Automotive.java
+++ src/model/Automotive.java
@@ -4,12 +4,53 @@ import java.util.ArrayList;
 import model.*;
 
 public class Automotive implements Serializable {
-    //separate variables to hold make and model
+    //separate variables to hold make and model, make + model = full name
     private String make;
     private String model;
     private float baseprice;
-    private ArrayList<OptionSet> opset;
-    private ArrayList<Option> choices;
+    private ArrayList<OptionSet> optionSet;
+    private ArrayList<Option> choices; //choices is an array list of options chosen
 
-    //TODO: tostring / print method
+    //constructors
+    public Automotive() {
+        this("", "", 0);
+    }
+
+    public Automotive(float baseprice) {
+        this("","", baseprice);
+    }
+
+    public Automotive(String make, String model, float baseprice ) {
+        this.make = make;
+        this.model = model;
+        this.baseprice = baseprice;
+        optionSet = new ArrayList<OptionSet>();
+        choices = new ArrayList<Option>();
+    }
+
+    //getters
+    public String getName() {
+        return(make + " " + model);
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public float baseprice() {
+        return baseprice;
+    }
+
+    public ArrayList getOpset() {
+        return optionSet;
+    }
+
+    //get opchoice, get opchoiceprice, set opchoice, get totalprice
+    //setters
+    //TODO: TOSTRING / PRINT
+
 }
