fe3bb8b8ede2a30f7bb9b85c4e62ab858b79faea
yenhoangle <yhle@ucsd.edu>
2019-11-11 18:29:19 -0800

    finished!

--- src/driver/driver3.java
+++ src/driver/driver3.java
@@ -2,7 +2,6 @@ package driver;
 
 import adapter.BuildAuto;
 import adapter.ConfigureAuto;
-import adapter.CreateAuto;
 import adapter.UpdateAuto;
 import exception.AutoException;
 import util.FileIO;
@@ -11,15 +10,17 @@ public class driver3 {
     public static void main(String[] args) throws AutoException {
         try {
             boolean complete = false;
-            String file = "C:\\Users\\Arteh\\IdeaProjects\\CIS35B_HW3\\textfiles\\ffw.txt";
+            //String file = "C:\\Users\\Arteh\\IdeaProjects\\CIS35B_HW3\\textfiles\\ffw.txt";
+            String file = "C:\\Users\\Arteh\\IdeaProjects\\CIS35B_HW3\\textfiles\\fes.txt";
             //a different file for a new car
             String file2 = "C:\\Users\\Arteh\\IdeaProjects\\CIS35B_HW3\\textfiles\\ffrs.txt";
             BuildAuto a1 = new BuildAuto();
             BuildAuto a2 = new BuildAuto();
             UpdateAuto ua = new BuildAuto();
             ConfigureAuto ca = new BuildAuto();
-            String name1 = "Ford Focus Wagon ZTW 2012";
-            String name2 = "Ford Focus RS 2017";
+            //String name1 = "Ford Focus Wagon ZTW 2012"; //name of first car
+            String name1 = "Ford Electric Scooter XL 2020"; //name of the first car
+            String name2 = "Ford Focus RS 2017"; //name of second car
 
             FileIO fileIO = new FileIO();
             //attempting to build the car
@@ -39,6 +40,8 @@ public class driver3 {
                     "Transmission", "Manual", -800);
             System.out.println("\nPrinting the second car after updating...");
             a2.printAuto(name2);
+
+            //choice operations
             System.out.println("\nTesting choice operations on the first car...");
             ca.selectChoices(name1);
             ca.printChoices(name1);
